4872a07cd83d5e3c0bd9d9a15a6c44fef137eac9
kai618 <vanhieu1601@gmail.com>
2020-10-14 00:57:22 +0700

    Improve PopoverButton

--- meal_app/src/components/PopoverButton.js
+++ meal_app/src/components/PopoverButton.js
@@ -1,5 +1,5 @@
 import React, { createRef } from 'react'
-import { Modal, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
+import { Modal, StyleSheet, TouchableWithoutFeedback, View, Dimensions } from 'react-native'
 
 export class PopoverButton extends React.Component {
     static defaultProps = {
@@ -56,6 +56,13 @@ export class PopoverButton extends React.Component {
 }
 
 export class Popover extends React.Component {
+    /**
+     * Render the transparent Popover first to get its dimensions via measure()
+     * Then determine whether the Popover exceeds the screen frame
+     * If yes, calculate new offset values
+     * Then set the Popover to the new position and make it visible
+     */
+
     static defaultProps = {
         offsetX: 0,
         offsetY: 5,
@@ -64,27 +71,46 @@ export class Popover extends React.Component {
     constructor(props) {
         super(props)
 
-        this.calculateOffset(this.props.position)
+        this.calculateInitialOffset(this.props.position)
     }
 
     state = {
         visible: this.props.visible || false,
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.comparePosition(prevProps.position, this.props.position))
-            this.calculateOffset(this.props.position)
+        hidden: false,
     }
 
     /**
      *
-     * @param {object} position
+     * @param {object} position { width, height, pageX, pageY }
      */
-    calculateOffset = (position) => {
-        this.offset = {
+    calculateInitialOffset = (position) => {
+        this.state.offset = {
             x: position.pageX + this.props.offsetX,
             y: position.pageY + position.height + this.props.offsetY,
         }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.comparePosition(prevProps.position, this.props.position))
+            this.calculateInitialOffset(this.props.position)
+    }
+
+    getPosition = () => {
+        this.popoverRef.measure((x, y, width, height) => {
+            // TODO: find a way to get status bar height
+            const totalHeight = height + this.state.offset.y + 20
+            const screenHeight = Dimensions.get('window').height
+            let diffOffsetY = screenHeight - totalHeight
+            if (diffOffsetY > 0) diffOffsetY = 0
+
+            this.setState((prevState) => ({
+                offset: {
+                    x: prevState.offset.x,
+                    y: prevState.offset.y + diffOffsetY,
+                },
+                hidden: false,
+            }))
+        })
     }
 
     /**
@@ -117,7 +143,18 @@ export class Popover extends React.Component {
                 <TouchableWithoutFeedback onPress={this.hide}>
                     <View style={styles.overlay} />
                 </TouchableWithoutFeedback>
-                <View style={[styles.wrapper, { top: this.offset.y, left: this.offset.x }]}>
+                <View
+                    style={[
+                        styles.wrapper,
+                        {
+                            top: this.state.offset.y,
+                            left: this.state.offset.x,
+                            opacity: this.state.hidden ? 0 : 1,
+                        },
+                    ]}
+                    ref={(ref) => (this.popoverRef = ref)}
+                    onLayout={() => this.getPosition()}
+                >
                     {this.props.children}
                 </View>
             </Modal>
